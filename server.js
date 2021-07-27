1b9b9439b9d8414976a6831f10f9b57c9ab1cff5
jaison alievi <jaison.alieve@gmail.com>
2021-07-27 12:51:11 -0300

    delay added

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router('db.json --delay=2000');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001;
+
+server.use(function (req, res, next) {
+  setTimeout(next, 2000);
+});
 
 server.use(middlewares);
 server.use(router);
